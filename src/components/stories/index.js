b1f01c25b5f78075cfa33f15024623ba479674f4
Shyam Gupta <sshyam@outlook.com>
2018-07-25 15:11:04 +0530

    Added jsconfig to disable IDE decorator warning

--- src/components/stories/index.js
+++ src/components/stories/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
 
 class Home extends React.Component {
   render() {
-    return <h1>asasdasd</h1>;
+    return <h1>asasdasd</h1>
   }
 }
 
-storiesOf("Home", module).add("to Storybook", () => <Home />);
+storiesOf('Home', module).add('to Storybook', () => <Home />)
