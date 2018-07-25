44a4562220711c2f2004e734031fefeeb042b0ac
Shyam Gupta <sshyam@outlook.com>
2018-07-25 15:00:20 +0530

    Added storybook

---
+++ src/components/stories/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+class Home extends React.Component {
+  render() {
+    return <h1>asasdasd</h1>;
+  }
+}
+
+storiesOf("Home", module).add("to Storybook", () => <Home />);
